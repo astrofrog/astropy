ae065fd039ae333bc019bece8eae9ca1581bbf47
Thomas Robitaille <thomas.robitaille@gmail.com>
2022-11-09 22:39:51 +0000

    Define minimal fitsio2.h

--- astropy/io/fits/tiled_compression/setup_package.py
+++ astropy/io/fits/tiled_compression/setup_package.py
@@ -4,13 +4,14 @@ import os
 
 from setuptools import Extension
 
+SRC_DIR = os.path.join(os.path.dirname(__file__), 'src')
+
 
 def get_extensions():
     return [Extension('astropy.io.fits.tiled_compression._compression',
-                      sources=[os.path.join(os.path.dirname(__file__),
-                                            'src', 'compression.c'),
+                      sources=[os.path.join(SRC_DIR, 'compression.c'),
                                os.path.join('cextern', 'cfitsio', 'lib', 'pliocomp.c'),
                                os.path.join('cextern', 'cfitsio', 'lib', 'ricecomp.c'),
                                os.path.join('cextern', 'cfitsio', 'lib', 'fits_hcompress.c'),
                                os.path.join('cextern', 'cfitsio', 'lib', 'fits_hdecompress.c')],
-                      include_dirs=[os.path.join('cextern', 'cfitsio', 'lib')])]
+                      include_dirs=[SRC_DIR])]
